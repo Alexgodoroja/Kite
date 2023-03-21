d4f219cbc2ad9f8f0816e191288d67e91b751031
Timman47 <timidisu@gmail.com>
2023-03-21 18:58:46 +0000

    Fixing Book model tests

--- blogs/tests/models/test_book_model.py
+++ blogs/tests/models/test_book_model.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from blogs.models import Book
+from blogs.models import books
 
 class BookModelTestCase(TestCase):
     """Unit tests of the book model"""
-    
+
     def setUp(self):
-        self.book = Book.objects.create(
-            isbn = "978-0547928202,
+        self.books = books.objects.create(
+            isbn = "978-0547928202",
             book_title = "The Hobbit",
             book_author = "J.R.R Tolkien",
             year_of_publication = "1937",
@@ -17,31 +17,27 @@ class BookModelTestCase(TestCase):
             image_url_m = "http://images.example.org/hobbit_medium.jpg",
             image_url_l = "http://images.example.org/hobbit_large.jpg"
         )
-        
+
     def _assert_book_is_valid(self):
         try:
-            self.book.full_clean()
+            self.books.full_clean()
         except ValidationError:
             self.fail("Test Book should be valid.")
-   
+
     def _assert_book_is_invalid(self):
         with self.assertRaises(ValidationError):
-            self.book.full_clean()
-        
-    def test_book_object_has_been_created(self):
-        self.assertTrue(isinstance(self.book, Book))
-        self.assertEqual(self.book.__str__(), self.book.book_title)
-        
+            self.books.full_clean()
+
     def test_book_has_necessary_fields(self):
-        self.assertEqual(self.book.isbn, "978-0547928202")
-        self.assertEqual(self.book.book_title, "The Hobbit")
-        self.assertEqual(self.book.book_author, "J.R.R Tolkien")
-        self.assertEqual(self.book.year_of_publication, "1937")
-        self.assertEqual(self.book.publisher, "Houghton MIfflin Harcourt")
-        self.assertEqual(self.book.image_url_s, "http://images.example.org/hobbit_small.jpg")
-        self.assertEqual(self.book.image_url_m, "http://images.example.org/hobbit_medium.jpg")
-        self.assertEqual(self.book.image_url_l, "http://images.example.org/hobbit_large.jpg")
-   
+        self.assertEqual(self.books.isbn, "978-0547928202")
+        self.assertEqual(self.books.book_title, "The Hobbit")
+        self.assertEqual(self.books.book_author, "J.R.R Tolkien")
+        self.assertEqual(self.books.year_of_publication, "1937")
+        self.assertEqual(self.books.publisher, "Houghton Mifflin Harcourt")
+        self.assertEqual(self.books.image_url_s, "http://images.example.org/hobbit_small.jpg")
+        self.assertEqual(self.books.image_url_m, "http://images.example.org/hobbit_medium.jpg")
+        self.assertEqual(self.books.image_url_l, "http://images.example.org/hobbit_large.jpg")
+
     def test_year_of_publication_cannot_exceed_four_digits(self):
-        self.book.year_of_publication = "19320"
+        self.books.year_of_publication = "19320"
         self._assert_book_is_invalid()
